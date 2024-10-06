0b8e78a3c59a9df6884db7b0e14cd4bd1520ee9e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-05 19:28:13 -0600

    fixed key error

--- src/app/certifications/page.tsx
+++ src/app/certifications/page.tsx
@@ -8,9 +8,9 @@ export default function Page() {
       <PageHeader title="Certifications" />
       <>
         {certs.map((cert, index) => (
-          <div className='bg-cyan-300 w-fit  rounded-md shadow-xl p-4'>
+          <div key={index} className='bg-cyan-300 w-fit  rounded-md shadow-xl p-4'>
             <div className="flex flex-col gap-2 rounded-md p-4 bg-[url('/paper.jpeg')] bg-cover">
-              <Image key={index} src={`/certs/${cert}`} alt={cert} width={600} height={600} />
+              <Image  src={`/certs/${cert}`} alt={cert} width={600} height={600} />
             </div>
           </div>
 
