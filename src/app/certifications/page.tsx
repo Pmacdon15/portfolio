b6c8bfa882776fa61e92537993a5f0706d0f334f
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-05 19:27:09 -0600

    added dynamic rendering of certification images

--- src/app/certifications/page.tsx
+++ src/app/certifications/page.tsx
@@ -1,8 +1,21 @@
 import PageHeader from "@/components/ui/page-header";
+import Image from "next/image";
+const certs = ['cSharp.png', 'networking.png', 'python.png',];
+
 export default function Page() {
   return (
     <>
-    <PageHeader title="Certifications" />
+      <PageHeader title="Certifications" />
+      <>
+        {certs.map((cert, index) => (
+          <div className='bg-cyan-300 w-fit  rounded-md shadow-xl p-4'>
+            <div className="flex flex-col gap-2 rounded-md p-4 bg-[url('/paper.jpeg')] bg-cover">
+              <Image key={index} src={`/certs/${cert}`} alt={cert} width={600} height={600} />
+            </div>
+          </div>
+
+        ))}
+      </>
     </>
   );
 };
