831cfe585c2e129f80953bbcfb1c5f6b1be2126b
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-21 12:55:07 -0600

    fixed nav bar bug

--- src/components/ui/nav-bar.tsx
+++ src/components/ui/nav-bar.tsx
@@ -13,25 +13,33 @@ export default function NavBar() {
     return (
         <div className='bg-cyan-300 bg-opacity-70 w-5/6 md:w-fit rounded-md shadow-xl md:hover:scale-105 transition-transform duration-200 p-2'>
             <button
-                className="md:hidden text-2xl hover:scale-110 transition-transform duration-200"
+                className="md:hidden text-2xl hover:scale-110 transition-transform duration-200 mx-auto my-2"
                 onClick={toggleMenu}
             >
-                <Image src="/Hamburger_icon.png" alt="Hamburger Icon" width={30} height={30} />
+                <Image 
+                className=''
+                src="/Hamburger_icon.png" alt="Hamburger Icon" width={30} height={30} />
             </button>
             <div className={`flex-col md:flex md:flex-row p-1 md:gap-8 text-xl ${isOpen ? 'flex bg-cyan-200 rounded-md' : 'hidden'}`}>
                 <Link
+                    onClick={toggleMenu}
                     href="/"
                     className='hover:scale-110  hover:underline transition-transform duration-200 '>About Me</Link>
                 <Link
+                    onClick={toggleMenu}
                     href="/projects"
                     className='hover:scale-110  hover:underline transition-transform duration-200'>Projects</Link>
                 <Link
+                    onClick={toggleMenu}
                     href="/transcript"
                     className='hover:scale-110 hover:underline transition-transform duration-200'>Transcript</Link>
                 <Link
+                    onClick={toggleMenu}
                     href="/resume"
                     className='hover:scale-110  hover:underline transition-transform duration-200'>Resume</Link>
-                <Link href="/certifications"
+                <Link
+                    onClick={toggleMenu}
+                    href="/certifications"
                     className='hover:scale-110 hover:underline transition-transform duration-200'>Certifications</Link>
             </div>
         </div>
